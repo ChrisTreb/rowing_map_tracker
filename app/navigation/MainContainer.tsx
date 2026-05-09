1e09c6d7ead7935bde545b749021add78e5fb975
chris <christophe.trebaol@gmail.com>
2026-05-09 11:51:49 +0200

    Removed tab navigation

--- app/navigation/MainContainer.tsx
+++ app/navigation/MainContainer.tsx
@@ -1,60 +1,25 @@
-import { Ionicons } from "@expo/vector-icons";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, StyleSheet, View } from "react-native";
 
 // Screens
 import EventsScreen from "./screens/EventsScreen";
 
-// TS interface for icon names
-type IconName = keyof typeof Ionicons.glyphMap;
-
-//Screen names
-const eventsName = "Evénements";
-
-const Tab = createBottomTabNavigator();
-
 function MainContainer() {
 
     return (
-        <>
+        <View style={styles.container}>
             <StatusBar animated={true} backgroundColor="#1E90FF" />
-
-            <Tab.Navigator
-                initialRouteName={eventsName}
-                screenOptions={({ route }) => ({
-                    tabBarActiveTintColor: "#1E90FF",
-                    tabBarInactiveTintColor: "grey",
-                    tabBarLabelStyle: {
-                        paddingBottom: 10,
-                        fontSize: 10,
-                    },
-                    tabBarStyle: [
-                        {
-                            paddingTop: 5,
-                            display: "flex",
-                        },
-                        null,
-                    ],
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName: IconName = "help-circle"; // fallback
-                        let rn = route.name;
-
-                        if (rn === eventsName) {
-                            iconName = focused ? "home" : "home-outline";
-                        }
-
-                        // You can return any component that you like here!
-                        return <Ionicons name={iconName} size={size} color={color} />;
-                    },
-                })}
-            >
-                <Tab.Screen name={eventsName} component={EventsScreen} />
-
-                { }
-            </Tab.Navigator>
-        </> // Utilisez un fragment React pour envelopper la StatusBar et le Tab.Navigator
+            <EventsScreen />
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        paddingVertical: 40,
+    },
+});
 
 export default MainContainer;
